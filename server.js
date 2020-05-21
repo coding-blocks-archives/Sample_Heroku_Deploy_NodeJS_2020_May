a16c492b09944671c3e8870d3d776dbfffb3480e
Arnav Gupta <arnav@codingblocks.com>
2020-05-21 21:45:35 +0530

    use heroku port
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
 
+const PORT = process.env.PORT || 4444
+
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
 
-app.listen(4444, (req, res) => {
-  console.log('started on http://localhost:4444')
+app.listen(PORT, (req, res) => {
+  console.log(`started on http://localhost:${PORT}`)
 })
